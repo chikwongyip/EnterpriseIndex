2e1faf0e899cbf09733c5b2006863d1e136e507b
chikwongyip <527439024@qq.com>
2023-06-16 16:49:33 +0800

    first commit

---
+++ src/App.vue
@@ -0,0 +1,25 @@
+<template>
+  <IndexPage>
+  </IndexPage>
+</template>
+
+<script>
+import IndexPage from "@/page/IndexPage";
+export default {
+  name: 'App',
+  components: {
+    IndexPage
+  }
+}
+</script>
+
+<style>
+/*#app {*/
+/*  font-family: Avenir, Helvetica, Arial, sans-serif;*/
+/*  -webkit-font-smoothing: antialiased;*/
+/*  -moz-osx-font-smoothing: grayscale;*/
+/*  text-align: center;*/
+/*  color: #2c3e50;*/
+/*  margin-top: 60px;*/
+/*}*/
+</style>
---
+++ src/page/IndexPage.vue
@@ -0,0 +1,33 @@
+<template>
+  <el-container class="header">
+    <el-header >
+       <MallHeader></MallHeader>
+    </el-header>
+    <el-main>
+      <MallMain></MallMain>
+    </el-main>
+    <el-footer>
+      <MallFooter></MallFooter>
+    </el-footer>
+  </el-container>
+</template>
+
+<script>
+import MallHeader from "@/components/MallHeader";
+import MallFooter from "@/components/MallFooter";
+import MallMain from "@/components/MallMain";
+export default {
+  name: "IndexPage",
+  components:{
+    MallHeader,
+    MallMain,
+    MallFooter
+  }
+}
+</script>
+
+<style scoped>
+  .header{
+    margin-top: 10px;
+  }
+</style>
