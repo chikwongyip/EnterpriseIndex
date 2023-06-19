d48001453e75a10acab72945251812b480a9950e
chikwongyip <527439024@qq.com>
2023-06-19 16:21:58 +0800

    header change

--- src/components/MallHeader.vue
+++ src/components/MallHeader.vue
@@ -1,29 +1,38 @@
 <template>
   <div>
     <div class="header">
-      <el-image :src="src">
-          <div slot="placeholder" class="image-slot">
-            加载中<span class="dot">...</span>
-          </div>
-      </el-image>
-      <el-input placeholder="输入名称搜索产品" class="input-with-select">
-        <el-select v-model="select" slot="prepend" placeholder="请选择">
-          <el-option label="产品名称" value="1"></el-option>
-          <el-option label="产品类型" value="2"></el-option>
-          <el-option label="产品品牌" value="3"></el-option>
-        </el-select>
-        <el-button slot="append" icon="el-icon-search"></el-button>
-      </el-input>
+      <div class="image">
+        <el-image 
+          :src="src"
+          :fit="fit"
+          style="width:200px; height:80px"
+          >
+            <div slot="placeholder" class="image-slot">
+              加载中<span class="dot">...</span>
+            </div>
+        </el-image>
+      </div>
+      <div class="searchBar">
+        <el-input placeholder="输入名称搜索产品" v-model="search" class="input-with-select">
+          <el-select v-model="select" slot="prepend" placeholder="请选择">
+            <el-option label="产品名称" value="1"></el-option>
+            <el-option label="产品类型" value="2"></el-option>
+            <el-option label="产品品牌" value="3"></el-option>
+          </el-select>
+          <el-button slot="append" icon="el-icon-search"></el-button>
+        </el-input>
+      </div>
     </div>
-    <div>
+    <div class="menu">
       <el-menu 
         :default-active="activeIndex" 
         class="el-menu-demo" 
         mode="horizontal" 
         @select="handleSelect" 
-        background-color="#759EFF"
-        text-color="#fff"
+        background-color="#1B438C"
+        text-color="#FDFFFD"
         active-text-color="#FFFEB1"
+        style="border-radius:15px"
         >
         <el-submenu index="1">
           <template slot="title">应用</template>
@@ -48,21 +57,23 @@
   </div>
 </template>
 <script>
-import companyInfo from "../api/index"
+import { companyInfo } from '@/api'
 export default {
   name: "MallHeader",
   data(){
     return{
-      baseUrl:"http://localhost:8000",
+      baseUrl:"http://localhost:8000/images/",
       src:"",
-      data:[]
+      data:[],
+      select:'',
+      search:""
     }
   },
   methods:{
     getData(){
-      companyInfo.then(res => {
-        this.data = res.data;
-        this.src = this.baseUrl+this.data[0].logo
+      companyInfo().then(res => {
+        this.data = res.data
+        this.src = this.baseUrl + this.data[0].logo;
       })
     }
   },
@@ -75,9 +86,22 @@ export default {
 <style scoped>
   .header{
     display: flex;
-    justify-content: left;
+    justify-content: space-around;
+    align-items: center;  
   }
-  .submenu{
-    color: white;
+  .image{
+    flex-basis: 10%;
+  }
+  .searchBar{
+    flex-basis: 70%;
+  }
+  .el-select .el-input{
+    width: 130px;
+  }
+  .input-with-select .el-input-group__prepend {
+    background-color: #fff;
+  }
+  .menu{
+    border-radius:25px
   }
 </style>
