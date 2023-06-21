adfec9269c47cbc95013c0c1d4e56097d8107594
chikwongyip <527439024@qq.com>
2023-06-21 21:59:22 +0800

    brand list

--- src/components/MallBrand.vue
+++ src/components/MallBrand.vue
@@ -1,57 +1,53 @@
 <template>
-    <div class="grid-container">
-      <div class="grid-item" 
-        v-for="item in items" 
-        :key="item"
-        :style="{ width: cellWidth + 'px', height: cellHeight + 'px' }" 
-        @click="handleClick(item)">
-        {{ item }}
-      </div>
+  <div class="grid-container">
+    <div class="grid-item" v-for="item in gridItems" :key="item.brand_id">
+      <img :src="item.brand_image" alt="" />
+      <p>{{ item.brand_name }}</p>
     </div>
-  </template>
-  
-  <script>
-  export default {
-    name: "MallBrand",
-    data() {
-      return {
-        items: [1, 2, 3, 4, 5, 6, 7, 8, 9]
-      }
-    },
-    computed: {
-      cellWidth() {
-        const containerWidth = this.$el.clientWidth;
-        // const cellCount = this.items.length;
-        return Math.floor(containerWidth / 3);
-      },
-      cellHeight() {
-        const containerHeight = this.$el.clientHeight;
-        // const cellCount = this.items.length;
-        return Math.floor(containerHeight / 3);
-      }
-    },
-    methods: {
-      handleClick(item) {
-        console.log(`Clicked item: ${item}`);
-      }
+  </div>
+</template>
+<script>
+import { getBrand } from '@/api';
+export default {
+  name: "MallBrand",
+  data() {
+    return {
+      gridItems:[],
+      baseUrl:"http://localhost:8000/images/"
     }
+  },
+  methods:{
+    getData(){
+      getBrand().then(res => {
+        this.gridItems = res.data.map((item) => {
+          item.brand_image = this.baseUrl+item.brand_image
+          return item
+        })
+      })
+    }
+  },
+  mounted(){
+    this.getData()
   }
-  </script>
+}
+</script>
   
-  <style scoped>
+<style scoped>
   .grid-container {
-    display: flex;
-    flex-wrap: wrap;
-    width: 100%;
-    height: 100%;
-    align-content: center;
-    justify-content: center;
+    display: grid;
+    grid-template-columns: repeat(auto-fit, minmax(250px, 1fr));
+    grid-gap: 16px;
   }
   
   .grid-item {
-    display: flex;
-    align-items: center;
-    justify-content: center;
     border: 1px solid #ccc;
+    border-radius: 4px;
+    text-align: center;
+    padding: 16px;
   }
-  </style>
+  
+  .grid-item img {
+    max-width: 50%;
+    height: auto;
+  }
+</style>
--- src/components/MallFooter.vue
+++ src/components/MallFooter.vue
@@ -1,33 +1,18 @@
 <template>
    <div>
-    <h1>探索我们的品牌</h1>
-     <div class="line"></div>
-     <el-col></el-col>
-     <el-row :gutter="20">
-       <el-col :span="5" v-for="item in brand" :key="item.brand_id">
-         <div class="brand">
-           <el-image :src="baseUrl+item.brand_image" style="width:200px;height:200px">
-            <div slot="placeholder" class="image-slot">
-              加载中<span class="dot">...</span>
-            </div>
-           </el-image>
-           <h3>
-            {{ item.brand_name }}
-           </h3>
-         </div>
-      </el-col>
-     </el-row>
+      <h1>探索我们的品牌</h1>
+      <div class="line"></div>
+      <MallBrand> </MallBrand>
    </div>
 </template>
 
 <script>
-import { getBrand } from '@/api';
-// import MallBrand from './MallBrand.vue';
+import MallBrand from './MallBrand.vue';
 export default {
   name: "MallFooter",
-  // components:{
-  //   MallBrand
-  // },
+  components:{
+    MallBrand
+  },
   data(){
     return{
       message:"",
@@ -35,16 +20,6 @@ export default {
       brand:[]
     }
   },
-  methods:{
-    getData(){
-      getBrand().then(res => {
-        this.brand = res.data
-      })
-    }
-  },
-  mounted(){
-    this.getData()
-  }
 }
 </script>
 
