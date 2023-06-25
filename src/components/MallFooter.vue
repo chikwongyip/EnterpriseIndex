db2685e8afa3271773d18cccfac1faa6232f10a3
chikwongyip <527439024@qq.com>
2023-06-25 15:56:35 +0800

    footer icon

--- src/components/MallFooter.vue
+++ src/components/MallFooter.vue
@@ -9,12 +9,34 @@
           <div class="col-md-4">
             <div class="single-feature">
               <div class="feature-icon">
-                <ion-icon :icon="heart"></ion-icon>
+                <PhoneIcon class="icon"></PhoneIcon>
               </div>
               <div class="feature-content">
                 <h3>(999) 1234 56789</h3>
-                <p>Free support line!</p>
+                <p>欢迎致电，垂询！</p>
+              </div>
             </div>
+          </div>
+          <div class="col-md-4">
+            <div class="single-feature">
+              <div class="feature-icon">
+                <MaillIcon class="icon"></MaillIcon>
+              </div>
+              <div class="feature-content">
+                <h3>(999) 1234 56789</h3>
+                <p>欢迎邮件报价！</p>
+              </div>
+            </div>
+          </div>
+          <div class="col-md-4">
+            <div class="single-feature">
+              <div class="feature-icon">
+                <LocationIcon class="icon"></LocationIcon>
+              </div>
+              <div class="feature-content">
+                <h3>(999) 1234 56789</h3>
+                <p>欢迎参观我们的团队</p>
+              </div>
             </div>
           </div>
         </div>
@@ -23,25 +45,32 @@
 </template>
 
 <script>
-import { heart } from 'ionicons/icons';
+import PhoneIcon from 'vue-ionicons/dist/md-call.vue'
+import MaillIcon from 'vue-ionicons/dist/md-mail.vue'
+import LocationIcon from 'vue-ionicons/dist/md-locate.vue'
 import MallBrand from './MallBrand.vue';
 export default {
   name: "MallFooter",
   components:{
-    MallBrand
+    MallBrand,
+    PhoneIcon,
+    LocationIcon,
+    MaillIcon
   },
   data(){
     return{
       message:"",
       baseUrl:"http://localhost:8000/images/",
       brand:[],
-      heart
     }
   },
 }
 </script>
 
 <style scoped>
+.container{
+  background-color: #1B438C;
+}
 .single-feature {
   display: -webkit-box;
   display: -ms-flexbox;
@@ -62,5 +91,8 @@ export default {
 	display: inline-block;
 	padding-left: 15px;
 	text-align: left;
+}
+.icon{
+  font-size: 80px;
 }
 </style>
--- src/main.js
+++ src/main.js
@@ -2,11 +2,9 @@ import Vue from 'vue'
 import ElementUI from "element-ui"
 import 'element-ui/lib/theme-chalk/index.css';
 import 'bootstrap/dist/css/bootstrap.css'
-import * as allIcons from 'ionicons/icons'
 import App from './App.vue'
 
 Vue.use(ElementUI)
-Vue.use(allIcons)
 Vue.config.productionTip = false
 
 new Vue({
