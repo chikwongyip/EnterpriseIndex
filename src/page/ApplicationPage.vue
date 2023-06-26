5d8a3bfbf4ae9b0f60af2a5b532efd0e3755f4d8
chikwongyip <527439024@qq.com>
2023-06-26 15:48:02 +0800

    router

---
+++ src/page/ApplicationPage.vue
@@ -0,0 +1,32 @@
+<template>
+    <el-container class="header">
+      <el-header >
+         <MallHeader></MallHeader>
+      </el-header>
+      <el-main>
+        <ApplicationMain></ApplicationMain>
+      </el-main>
+      <el-footer>
+        <MallFooter></MallFooter>
+      </el-footer>
+    </el-container>
+  </template>
+  <script>
+  import MallHeader from "@/components/MallHeader";
+  import MallFooter from "@/components/MallFooter";
+  import ApplicationMain from "@/components/ApplicationMain.vue";
+  export default {
+    name: "ApplicationPage",
+    components:{
+      MallHeader,
+      ApplicationMain,
+      MallFooter
+    }
+  }
+  </script>
+  
+  <style scoped>
+    .header{
+      margin-top: 10px;
+    }
+  </style>
