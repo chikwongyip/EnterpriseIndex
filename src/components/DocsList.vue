280874fc794bb7c23d8a065269144b193d206001
chikwongyip <527439024@qq.com>
2023-10-06 08:40:25 +0800

    document check

--- src/components/DocsList.vue
+++ src/components/DocsList.vue
@@ -2,15 +2,16 @@
     <div class="container">
         <div class="row g-4">
             <div class="col-md-4" v-for="item in documentList" :key="item.id">
-                <b-card :title="item.name">
+                <b-card :title="item.name" @click="cardListOnClick(item.docs)">
                     <b-card-text>
                         <b-icon-file-earmark-pdf></b-icon-file-earmark-pdf>
-                        <span>test.pdf</span>
+                        <span>{{ item.name + ".pdf" }}</span>
                     </b-card-text>
                     <div class="d-flex gap-2 small text-muted">
-                        <span>1 May</span>
-                        <span>20 KB</span>
-                        <span>2 Members</span>
+                        <span>{{ "上传时间：" + item.date }}</span>
+                    </div>
+                    <div class="d-flex gap-2 small text-muted">
+                        <span>{{ "文件大小：" + item.size + "m" }}</span>
                     </div>
                 </b-card>
             </div> 
